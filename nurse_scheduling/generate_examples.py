b922f920c1ad4af0b86053438c82902d07442bbc
Matheus Xavier Sampaio <matheus.sampaio011@gmail.com>
2019-11-16 13:00:34 -0300

    renamed folders

--- nurse_scheduling/generate_examples.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with open('testes.txt', 'w') as f:
-    for i in range(10):
-        zeros = np.zeros(210, dtype=int)
-        ones = np.random.choice(210, np.random.randint(210))
-        zeros[ones] = 1
-        print(''.join([str(n) for n in zeros]), file=f)
